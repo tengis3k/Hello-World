000c9f008f483a6f318461cba58ba46b1fd67e50
tengis3k <167059521+tengis3k@users.noreply.github.com>
2024-04-15 14:15:13 +0800

    Add files via upload

---
+++ whitepaper/script.js
@@ -0,0 +1,8 @@
+function startAnimation() {
+  var paper = document.getElementById("paper-img");
+  var content = document.getElementById("content");
+
+  // Add classes to trigger animations
+  paper.classList.add("move-animation");
+  content.classList.add("show-content");
+}
